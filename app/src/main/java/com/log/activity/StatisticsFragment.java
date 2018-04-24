b03e76a3141800b338026857011ff1014667026f
Bartosz Górski <bartoszg16@gmail.com>
2018-04-24 21:21:04 +0200

    Add bavigation menu and google log in fragment

---
+++ app/src/main/java/com/log/activity/StatisticsFragment.java
@@ -0,0 +1,19 @@
+package com.log.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StatisticsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_statistics, container, false);
+
+        return view;
+    }
+
+}
