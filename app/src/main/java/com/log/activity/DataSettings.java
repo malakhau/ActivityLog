16925f283825fa7628c91fd2dffaf8f37a975838
Bartosz Górski <bartoszg16@gmail.com>
2018-04-29 12:45:37 +0200

    Highlight bmi text

--- app/src/main/java/com/log/activity/DataSettings.java
+++ app/src/main/java/com/log/activity/DataSettings.java
@@ -1,5 +1,6 @@
 package com.log.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class DataSettings extends Fragment {
 
@@ -16,6 +18,9 @@ public class DataSettings extends Fragment {
     private EditText weightEditText;
     private EditText BMIeditText;
     private ProgressBar BMIbar;
+    private TextView underweightText;
+    private TextView healthyweightText;
+    private TextView overweightText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -49,7 +54,25 @@ public class DataSettings extends Fragment {
         int multiplierProgressBar = 4;
         int bmiProgressBar = Math.round((bmi - minValueOfBMI) * multiplierProgressBar);
         BMIbar.setProgress(bmiProgressBar);
+        changeTextColorDependsUponBMI(bmi);
     }
 
+    private void changeTextColorDependsUponBMI(float aBMI) {
+        resetBMItextCollorToDefault();
 
+        if (aBMI < 18.5) {
+            underweightText.setTextColor(Color.RED);
+        } else if (aBMI >= 18.5 && aBMI <= 25.0) {
+            healthyweightText.setTextColor(Color.GREEN);
+        } else if (aBMI > 25.0) {
+            overweightText.setTextColor(Color.RED);
+        }
+    }
+
+    private void resetBMItextCollorToDefault() {
+        int defaultColor = getResources().getColor(android.R.color.darker_gray);
+        underweightText.setTextColor(defaultColor);
+        healthyweightText.setTextColor(defaultColor);
+        overweightText.setTextColor(defaultColor);
+    }
 }
