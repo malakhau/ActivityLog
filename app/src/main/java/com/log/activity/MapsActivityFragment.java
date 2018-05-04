5e1a2251cb9bb1a8cb684e31858a60174e03275d
Luka Malakhau <l.malakhau@gmail.com>
2018-05-04 14:27:56 +0200

    Hot fix

--- app/src/main/java/com/log/activity/MapsActivityFragment.java
+++ app/src/main/java/com/log/activity/MapsActivityFragment.java
@@ -61,9 +61,13 @@ public class MapsActivityFragment extends Fragment implements OnMapReadyCallback
             public void onLocationChanged(Location location) {
 
                 LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
-                mMap.clear();
-                mMap.addMarker(new MarkerOptions().position(latLng).title("Your current location"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+                if (mMap != null){
+                    mMap.clear();
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("Your current location"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+                    //mMap.addPolyline();
+                }
+
             }
 
             @Override
