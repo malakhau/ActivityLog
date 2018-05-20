023d7977ae8e73b3e09c58470dcb308af5f6d177
Bartosz Górski <bartoszg16@gmail.com>
2018-05-20 21:04:44 +0200

    Add Fake Points. Fill Location Recorder methods

--- app/src/main/java/com/log/activity/LocationRecorder.java
+++ app/src/main/java/com/log/activity/LocationRecorder.java
@@ -13,10 +13,14 @@ public class LocationRecorder {
     }
 
     float distanceBetweenTwoPoints(PointF point1, PointF point2) {
-        return 0f;
+        return (float)(Math.sqrt(Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2)));
     }
 
     float entireDistance(List<PointF> points) {
-        return 0f;
+        float entireDistance = 0f;
+        for(int i=0; i+1 < points.size(); i++) {
+            entireDistance += distanceBetweenTwoPoints(points.get(i), points.get(i+1));
+        }
+        return entireDistance;
     }
 }
--- app/src/test/java/com/log/activity/CalculateDistanceUnitTests.java
+++ app/src/test/java/com/log/activity/CalculateDistanceUnitTests.java
@@ -1,7 +1,6 @@
 package com.log.activity;
 
 import android.graphics.PointF;
-import android.test.suitebuilder.annotation.SmallTest;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,23 +12,29 @@ import java.util.List;
 import static junit.framework.Assert.assertEquals;
 
 
-@SmallTest
 public class CalculateDistanceUnitTests {
 
     private LocationRecorder lr;
-    private List<PointF> locations;
+    private List<FakePointF> locations;
+
+    public static class FakePointF extends PointF {
+        FakePointF(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     @Before
     public void setUp() {
         lr = new LocationRecorder();
         locations = new ArrayList<>(
                 Arrays.asList(
-                        new PointF(1f, 2f),
-                        new PointF(1f, 5f),
-                        new PointF(0f, 4f),
-                        new PointF(4f, 2f),
-                        new PointF(-2f, -2f),
-                        new PointF(2f, 2f)
+                        new FakePointF(1f, 2f),
+                        new FakePointF(1f, 5f),
+                        new FakePointF(0f, 4f),
+                        new FakePointF(4f, 2f),
+                        new FakePointF(-2f, -2f),
+                        new FakePointF(2f, 2f)
                 )
         );
     }
@@ -43,6 +48,6 @@ public class CalculateDistanceUnitTests {
 
     @Test
     public void calculateEntireDistance_isCorrect() {
-        assertEquals(13.128990204f, lr.entireDistance(locations));
+        assertEquals(21.754306317f, lr.entireDistance(new ArrayList<PointF>(locations)));
     }
 }
