cdd27173cec120c63ad738ea29d7c03968026457
Bartosz Górski <bartoszg16@gmail.com>
2018-05-23 18:51:00 +0200

    Add point class

---
+++ app/src/main/java/com/log/activity/Point.java
@@ -0,0 +1,16 @@
+package com.log.activity;
+
+
+import java.util.Date;
+
+public class Point {
+    public float x;
+    public float y;
+    public Date currentTime;
+
+    Point(float x, float y, Date currentTime) {
+        this.x = x;
+        this.y = y;
+        this.currentTime = currentTime;
+    }
+}
