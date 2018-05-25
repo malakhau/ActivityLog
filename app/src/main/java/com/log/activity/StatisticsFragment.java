5afc89bb77ef86c58ce1f2fbc6cb9666eaee8759
Luka Malakhau <l.malakhau@gmail.com>
2018-05-25 18:00:14 +0200

    Doesn't work as should

--- app/src/main/java/com/log/activity/StatisticsFragment.java
+++ app/src/main/java/com/log/activity/StatisticsFragment.java
@@ -1,19 +1,112 @@
 package com.log.activity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class StatisticsFragment extends Fragment {
+
+    public Button startButton, stopButton;
+    public BroadcastReceiver broadcastReceiver;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        startButton = getActivity().findViewById(R.id.startTracking);
+        stopButton = getActivity().findViewById(R.id.stopTracking);
+
+        if(!runtime_permissions()){}
+            enable_buttons();
         View view = inflater.inflate(R.layout.fragment_statistics, container, false);
 
         return view;
+    }
+
+    private void enable_buttons() {
+
+        stopButton.setEnabled(false);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(getContext().getApplicationContext(),GPS_Service.class);
+                getActivity().startService(i);
+                startButton.setEnabled(false);
+                stopButton.setEnabled(true);
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(getContext().getApplicationContext(),GPS_Service.class);
+                getActivity().stopService(i);
+                stopButton.setEnabled(false);
+                startButton.setEnabled(true);
+            }
+        });
+
+    }
+
+    private boolean runtime_permissions() {
+        if(Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION},100);
+
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 100){
+            if( grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                enable_buttons();
+            }else {
+                runtime_permissions();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(broadcastReceiver == null){
+            broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+
+                    Log.i("location","\n" +intent.getExtras().get("coordinates"));
+
+                }
+            };
+        }
+        getContext().registerReceiver(broadcastReceiver,new IntentFilter("location_update"));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(broadcastReceiver != null){
+            getContext().unregisterReceiver(broadcastReceiver);
+        }
     }
 
 }
