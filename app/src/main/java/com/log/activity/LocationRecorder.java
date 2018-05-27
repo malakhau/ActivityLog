12dc98f7b16c269f113ecb4495077e03de104c80
Bartosz Górski <bartoszg16@gmail.com>
2018-05-27 22:42:33 +0200

    Add point getter to Location Recorder

--- app/src/main/java/com/log/activity/LocationRecorder.java
+++ app/src/main/java/com/log/activity/LocationRecorder.java
@@ -83,4 +83,8 @@ public class LocationRecorder {
     float getAvrageVelocityInMetersPerSecund() {
         return mAvrageVelocityInMetersPerSecund;
     }
+
+    public List<Point> getPoints() {
+        return mPoints;
+    }
 }
