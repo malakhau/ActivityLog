d26fec563beb7eadba1e2dd480f46c9698b4961d
Luka Malakhau <l.malakhau@gmail.com>
2018-06-06 20:05:57 +0200

    Demo plot

--- app/src/main/java/com/log/activity/PlotSteps.java
+++ app/src/main/java/com/log/activity/PlotSteps.java
@@ -8,6 +8,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -27,6 +31,16 @@ public class PlotSteps extends AppCompatActivity {
         backButt = findViewById(R.id.backToActivity);
         Toast.makeText(this, "Date:" + thisDate, Toast.LENGTH_SHORT).show();
         back();
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
     }
 
     public void back(){
