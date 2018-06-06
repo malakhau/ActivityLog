f77001ff733bab5624e8931a1bc24d5a62005a53
Luka Malakhau <l.malakhau@gmail.com>
2018-06-06 20:26:56 +0200

    Switched to BarGraph

--- app/src/main/java/com/log/activity/PlotSteps.java
+++ app/src/main/java/com/log/activity/PlotSteps.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
@@ -33,7 +34,7 @@ public class PlotSteps extends AppCompatActivity {
         back();
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<DataPoint>(new DataPoint[] {
                 new DataPoint(0, 1),
                 new DataPoint(1, 5),
                 new DataPoint(2, 3),
